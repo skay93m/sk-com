87a740275a0e9cae31d46dfd76bed42a1ef4f551
Syafiq <syafiq@syafiqkay.com>
2025-08-10 16:32:35 +0000

    feat(home): add new style

--- home/views.py
+++ home/views.py
@@ -5,9 +5,9 @@ def home(request):
     return render(request, 'index.html', {
         'title': 'Home',
         'header': 'Welcome!',
-        'image': 'img/img001.jpg',
-        'statement1': 'Hi I am Syafiq and welcome to my personal website.', # who am I
-        'statement2': 'I wrote this website using Django and Bootstrap using Visual Studio Code.', # what I do
-        'statement3': 'I want to use this website as a platform for exploring my curiousity, sharing my works and connecting with others.', # why I do it
+        'statement1': 'Pharmacist → Aspiring Barrister | Tech Enthusiast | Lifelong Learner', # who am I
+        'statement2': (
+            "My professional journey began in the field of pharmacy, where I developed a strong foundation in healthcare, patient advocacy, and scientific rigor. Driven by a passion for justice and a desire to make a broader impact, I transitioned towards law, aspiring to become a barrister. This shift has allowed me to combine my analytical skills and ethical commitment from pharmacy with the critical thinking and advocacy required in legal practice. Along the way, I discovered a deep interest in technology, recognizing its transformative potential in both healthcare and law. I have actively pursued opportunities to expand my technical skills, engaging with projects that bridge the gap between these disciplines. Lifelong learning is at the core of my journey, motivating me to continuously seek new knowledge and adapt to evolving challenges. My diverse background enables me to approach problems from multiple perspectives, fostering innovative solutions and meaningful contributions. Whether working on legal cases, healthcare initiatives, or tech-driven projects, I am committed to making a positive difference and inspiring others to embrace interdisciplinary growth."
+        ), # what I do
         'cta_button': 'Explore my projects' # call to action
     })
