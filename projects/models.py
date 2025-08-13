462b119e0b67d3a8f34720d0af142a7750a5ce72
Syafiq <syafiq@syafiqkay.com>
2025-08-13 19:57:58 +0000

    feat(projects): added projects app

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    CATEGORY_CHOICES = [
+        ('now', 'Current'),
+        ('backburner', 'Backburner'),
+        ('someday', 'Someday'),
+    ]
+
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='someday')
+    description = models.TextField(blank=False, help_text='Explain what you intend to accomplish with the project.')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Project
+from .forms import ProjectForm
 
-# Create your views here.
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'project_list.html', {'projects': projects})
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'project_detail.html', {'project': project})
+
+def project_create(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save()
+            return redirect('projects:project_detail', pk=project.pk)
+    else:
+        form = ProjectForm()
+    return render(request, 'project_create.html', {'form': form})
+
+def project_edit(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects:project_detail', pk=project.pk)
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, 'project_edit.html', {'form': form, 'project': project})
+
+def project_delete(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects:project_home')
+    return render(request, 'project_delete.html', {'project': project})
