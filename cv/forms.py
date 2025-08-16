91beeb83c0acf36b24c1543c913de630456151b2
Syafiq Kay <syafiq@syafiqkay.com>
2025-08-16 17:27:34 +0100

    feat: implement credential management with create, edit, and delete functionalities

--- cv/forms.py
+++ cv/forms.py
@@ -1,5 +1,39 @@
 from django import forms
+from .models import Credentials
 
-class CredentialForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    details = forms.CharField(widget=forms.Textarea)
+class CredentialForm(forms.ModelForm):
+    '''Form for creating and updating credentials.'''
+    class Meta:
+        model = Credentials
+        fields = [
+            'title',
+            'institution',
+            'date_obtained', 
+            'icon', 
+            'link' 
+        ]
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter the credential'
+            }),
+            'institution': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'awarding institution'
+            }),
+            'date_obtained': forms.DateTimeInput(attrs={
+                'class': 'form-control',
+                'type': 'datetime-local'
+            }),
+            'icon': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Filename of the icon in cv/static/icon'
+            }),
+            'link': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': "URL of the credential's official page"
+            })
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
--- cv/views.py
+++ cv/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Credentials
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import CredentialForm
 
 def cv_main(request):
     context = {
@@ -7,3 +10,74 @@ def cv_main(request):
     }
 
     return render(request, 'cv_main.html', context)
+
+def credential_list(request):
+    '''Display list of all credentials'''
+    credentials = Credentials.objects.all
+    return render(request, 'credential_detail.html', context={credentials,})
+
+@login_required
+def credential_create(request):
+    '''Create a new credential'''
+    if request.method == 'POST':
+        form = CredentialForm(request.POST)
+        if form.is_valid():
+            credential = form.save(commit=False)
+            credential.save()
+            messages.success(request, f'Credential "{credential.title}" created successfully!')
+            return redirect('cv:credential_detail', pk=credential.pk)
+    else:
+        form = CredentialForm()
+    
+    context = {
+        'form': form,
+        'title': 'Create New Credential',
+    }
+    return render(request, 'credential_form.html', context)
+
+@login_required
+def credential_edit(request, pk):
+    credential = get_object_or_404(Credentials, pk=pk)
+    
+    # only allow staff to edit
+    if credential.author != request.user and not request.user.is_staff:
+        messages.error(request, 'You do not have permission to edit this writing.')
+        return redirect('cv:cv_main')
+    
+    if request.method == 'POST':
+        form = CredentialForm(request.POST, instance=credential)
+        if form.is_valid():
+            credential = form.save(commit=False)
+            credential.save()
+            messages.success(request, f'Credential "{credential.title}" updated successfully!')
+            return redirect('cv:credential_detail', pk=credential.pk)
+    else:
+        form = CredentialForm(instance=credential)
+        
+    context = {
+        'form': form,
+        'credential': credential,
+        'title': f'Edit: {credential.title}',
+    }
+    return render(request, 'credential_form.html', context)
+
+@login_required
+def credential_delete(request, pk):
+    '''Delete a credential'''
+    credential = get_object_or_404(Credentials, pk=pk)
+    
+    # only allow author or staff to delete
+    if credential.author != request.user and not request.user.is_staff:
+        messages.error(request, 'You do not have permission to delete credentials.')
+        return redirect('cv:credential_detail', pk=pk)
+    
+    if request.method == 'POST':
+        title = credential.title
+        credential.delete()
+        messages.success(request, f'Writing "{title}" deleted successfully!')
+        return redirect('cv:cv_main')
+    
+    context = {
+        'credential': credential,
+    }
+    return render(request, 'credential_delete.html', context)
